fa881905cd7dc56d702e49f941f9be6f85135220
ymdysk <ymdysk@sic.shimano.co.jp>
2022-10-13 11:58:17 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yusuke/taskapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package jp.techacademy.yusuke.taskapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mTaskAdapter: TaskAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        // ListViewの設定
+        mTaskAdapter = TaskAdapter(this)
+
+        // ListViewをタップしたときの処理
+        listView1.setOnItemClickListener { parent, view, position, id ->
+            // 入力・編集する画面に遷移させる
+        }
+
+        // ListViewを長押ししたときの処理
+        listView1.setOnItemLongClickListener { parent, view, position, id ->
+            // タスクを削除する
+            true
+        }
+
+        reloadListView()
+    }
+
+    private fun reloadListView() {
+        // 後でTaskクラスに変更する
+        val taskList = mutableListOf("aaa", "bbb", "ccc")
+
+        mTaskAdapter.mTaskList = taskList
+        listView1.adapter = mTaskAdapter
+        mTaskAdapter.notifyDataSetChanged()
+    }
+}
